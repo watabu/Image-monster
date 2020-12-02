62a2fa86b8502376930d5b5752d3c5f268cafe87
watabu <4618085@ed.tus.ac.jp>
2020-12-02 10:36:43 +0900

    Add: モンスター生成画面の大枠を作成
    画像読み込み、画像表示までを作成した。
    画像表示の方法がよくわからないがclassで簡単に表示できるようにした。
    もっと簡単な方法があったら教えてください！！！

---
+++ sorces/View.py
@@ -0,0 +1,46 @@
+import sys, os
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+import random
+import cv2
+import numpy as np
+
+class ImageViewer:
+    def __init__(self, parent, image=None, x=0, y=0):
+        #super(ImageViewer, self).__init__(parent)
+        self.parent = parent
+        self.imageLabel = QLabel(parent=self.parent)
+        self.imageLabel.move(x, y)
+        self.w = 0
+        self.h = 0
+        self.fixSize = False
+        if(image is None):
+            self.image = None
+            return 
+        self.setImage(image)
+        self.image = image
+    
+    def setImage(self, image):
+        if self.fixSize:
+            image = cv2.resize(image , dsize=(self.w , self.h))
+        
+        height, width, dim = image.shape
+        bytesPerLine = dim * width        
+        self.imageLabel.resize(width, height)
+
+        self.imageLabel.resize(width, height)
+        image = QImage(image, width, height, bytesPerLine, QImage.Format_RGB888)
+        self.imageLabel.setPixmap(QPixmap.fromImage(image))
+        self.image = image
+
+    #画像の表示位置を設定する
+    def move(self, x, y):
+        self.imageLabel.move(x,y)
+
+    #あらかじめ画像が表示される大きさを設定する
+    def resize(self, w, h):
+        self.fixSize = True
+        self.w , self.h = w, h
+        if  not(self.image is None): 
+            self.setImage(self.image)
---
+++ sorces/main.py
@@ -0,0 +1,110 @@
+import sys, os
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+import random
+import cv2
+import numpy as np
+from View import ImageViewer
+
+#画像取り込み、切り抜き、ステータス生成をする
+class MonsterGenerator:
+    def __init__(self, parent):
+        self.parent = parent
+        
+    #画像を取得し親に渡す
+    def capture(self):
+        # ファイル選択ダイアログの表示
+        file_name = QFileDialog.getOpenFileName(self.parent, 'Open file', './')     # 画像を選択してファイル名を取得
+        if file_name[0] == '':
+            return    
+       
+        n = np.fromfile(file_name[0], dtype=np.uint8)# imreadだと日本語のファイル名に対応できないため，np.fromfileとcv2.imdecodeを使う
+        image = cv2.imdecode(n, cv2.IMREAD_COLOR) 
+        self.image = image
+        #画像をセットする
+        self.parent.setImage(image)
+
+    #画像を切り抜く
+    def crop(self, image):
+        #TODO 切り抜き方法を工夫する
+        height, width, dim = image.shape
+        return image[height//4 :height*3//4, width//4 :width*3//4,:]
+
+    #ステータスを生成して返す
+    def generateStatus(self, image):
+        #TODO 画像によってステータスを生成する
+        return Status()
+
+    def generateMonster(self, image):
+        monster = Monster(self.crop(image))
+        monster.status = self.generateStatus(image)
+        return monster
+
+
+
+class Status:
+    def __init__(self, hp=0, attack=0, defence=0):
+        self.hp = 0
+        self.attack = 0
+        self.defence = 0
+    
+class Battle:
+    pass
+
+class Monster:
+    def __init__(self, image):
+        self.status = Status()
+        self.image = image
+
+
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.generator = MonsterGenerator(self)
+    
+        self.initUI()
+
+    def initUI(self):
+        self.resize(800, 600) 
+        self.setWindowTitle('Game')
+        #ボタン生成・クリック時の挙動、座標設定
+        captureButton = QPushButton('モンスター生成', self)
+        captureButton.clicked.connect(self.generator.capture)
+        captureButton.move(80,20)
+
+        #文字生成？
+        label = QLabel("test", self)
+        label.move(200, 20)
+
+        #画像生成　self.show()の前に作っておかないと表示されない＜ーなんで？
+        self.imageViewer = ImageViewer(self, x=50, y=100)
+        #画像が表示される大きさを指定
+        self.imageViewer.resize(300, 300)
+
+        self.iconViewer = ImageViewer(self, x=450, y=100)
+        #画像が表示される大きさを指定
+        self.iconViewer.resize(200, 200)
+
+        self.show()
+
+    def setImage(self, image):
+        #画像を選択後に呼び出される
+        #元画像と切り抜き後の画像をセットする
+        self.image = image
+        self.player = self.generator.generateMonster(self.image)
+
+        self.imageViewer.setImage(image)
+        self.iconViewer.setImage(self.player.image)
+        
+        
+        
+
+def main():
+    app = QApplication(sys.argv)
+    w = MyWindow()
+    app.exec_()
+            
+if __name__ == '__main__':
+    main()
